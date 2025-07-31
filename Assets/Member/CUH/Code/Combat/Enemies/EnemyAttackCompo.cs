47e54e0806d360f0c282e89e4f7012310e81999f
chuh007 <chhuh007@gmail.com>
2025-07-31 11:47:09 +0900

    feat : AttackCompo

---
+++ Assets/Member/CUH/Code/Combat/Enemies/EnemyAttackCompo.cs
@@ -0,0 +1,22 @@
+﻿using Member.CUH.Code.Enemies;
+using Member.CUH.Code.Entities;
+using UnityEngine;
+
+namespace Member.CUH.Code.Combat.Enemies
+{
+    public abstract class EnemyAttackCompo : MonoBehaviour, IEntityComponent
+    {
+        protected Enemy _enemy;
+        private float _lastAtkTime;
+        
+        public virtual void Initialize(Entity entity)
+        {
+            _enemy = entity as Enemy;
+        }
+
+        public virtual void Attack()
+        {
+            _lastAtkTime = Time.time;
+        }
+    }
+}

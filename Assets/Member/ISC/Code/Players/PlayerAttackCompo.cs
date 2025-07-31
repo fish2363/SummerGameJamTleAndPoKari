4b277fc503048cb9206c61ab196d3c1b53975ce6
Candy <dltkr0424@gmail.com>
2025-07-31 19:37:25 +0900

    test : AttackDebuggTest

--- Assets/Member/ISC/Code/Players/PlayerAttackCompo.cs
+++ Assets/Member/ISC/Code/Players/PlayerAttackCompo.cs
@@ -6,19 +6,49 @@ namespace Member.ISC.Code.Players
 {
     public class PlayerAttackCompo : MonoBehaviour, IEntityComponent
     {
+        [SerializeField] private LayerMask whatIsTarget;
+        
         [SerializeField] private float castRadius;
         [SerializeField] private float parryRadius;
 
         private bool isParry;
 
         private Player _player;
+
+        private Collider2D[] targetArr;
         
         public void Initialize(Entity entity)
         {
             _player = entity as Player;
         }
-        
-        #if UNITY_EDITOR
+
+        [ContextMenu("테스트용 어택")]
+        public void Attack()
+        {
+            int c = Physics2D.OverlapCircleNonAlloc(_player.transform.position, castRadius, targetArr, whatIsTarget);
+
+            if (c > 0)
+            {
+                foreach (Collider2D item in targetArr)
+                {
+                    if (InSight(item.transform.position))
+                    {
+                        Debug.Log("앞!");
+                    }
+                }
+            }
+        }
+
+        private bool InSight(Vector3 target)
+        {
+            Vector3 dir = _player.transform.position - target;
+
+            float value = Vector3.Dot(dir, target);
+
+            return value > 0;
+        }
+
+#if UNITY_EDITOR
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;

2bb2114c996b555394a5ff56fe1ab4af7a834848
Candy <dltkr0424@gmail.com>
2025-07-31 18:58:17 +0900

    test : PlayerAttackDebugging

--- Assets/Member/ISC/Code/Players/PlayerAttackCompo.cs
+++ Assets/Member/ISC/Code/Players/PlayerAttackCompo.cs
@@ -1,16 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using Member.CUH.Code.Entities;
+using UnityEngine;
 
 namespace Member.ISC.Code.Players
 {
-    public class PlayerAttackCompo : MonoBehaviour
+    public class PlayerAttackCompo : MonoBehaviour, IEntityComponent
     {
         [SerializeField] private float castRadius;
+        [SerializeField] private float parryRadius;
 
         private bool isParry;
+
+        private Player _player;
         
-        public void Attack()
+        public void Initialize(Entity entity)
         {
-            
+            _player = entity as Player;
         }
+        
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, castRadius);
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, castRadius - parryRadius);
+        }
+        #endif
     }
 }

d585d9769ce1dea6d39eefab522e5baa083f4da3
fish2363 <a01022639410@gmail.com>
2025-08-01 02:05:36 +0900

    fix : shootSequence

--- Assets/Member/CUH/Code/Combat/Enemies/EnemyAttackCompo.cs
+++ Assets/Member/CUH/Code/Combat/Enemies/EnemyAttackCompo.cs
@@ -10,8 +10,6 @@ namespace Member.CUH.Code.Combat.Enemies
     {
         [SerializeField] private float attackRange = 100f;
         [SerializeField] private float attackCooldown = 1f;
-        [Header("뽀용 대기")]
-        [SerializeField] private float attackWait;
 
         protected Enemy _enemy;
         protected IDamageable _target;
@@ -41,7 +39,7 @@ namespace Member.CUH.Code.Combat.Enemies
             Debug.Log("저놈추");
             _enemy.DOKill();
 
-            if (shootSequence.IsActive()) DOVirtual.DelayedCall(1f,()=> shootSequence.Restart()); // 매번 새로 만들지 않고 재시작
+            if (shootSequence.IsActive()) shootSequence.Restart(); // 매번 새로 만들지 않고 재시작
 
             _lastAtkTime = Time.time;
         }

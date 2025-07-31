ba0fe381e01bd367e5d37c1219004ce226893eb4
Candy <dltkr0424@gmail.com>
2025-07-31 12:13:11 +0900

    feat : InputManager, Player

---
+++ Assets/Member/ISC/Code/Players/Player.cs
@@ -0,0 +1,18 @@
+﻿using Member.CUH.Code.Entities;
+using Member.ISC.Code.Managers;
+using UnityEngine;
+
+namespace Member.ISC.Code.Players
+{
+    public class Player : Entity
+    {
+        [field: SerializeField] public InputManagerSO PlayerInput { get; private set; }
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            
+        }
+    }
+}
---
+++ Assets/Member/ISC/Code/Players/PlayerMoveCompo.cs
@@ -0,0 +1,28 @@
+﻿using Member.CUH.Code.Entities;
+using UnityEngine;
+
+namespace Member.ISC.Code.Players
+{
+    public class PlayerMoveCompo : MonoBehaviour, IEntityComponent
+    {
+        [SerializeField] private Rigidbody2D rb; 
+        
+        [Header("이동속도 및 회전속도")]
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float rotationSpeed;
+        
+        private Player _player;
+
+        private Vector2 _moveDir;
+        
+        public void Initialize(Entity entity)
+        {
+            _player = entity as Player;
+        }
+
+        public void SetDirection(Vector2 dir)
+        {
+            _moveDir = dir;
+        }
+    }
+}

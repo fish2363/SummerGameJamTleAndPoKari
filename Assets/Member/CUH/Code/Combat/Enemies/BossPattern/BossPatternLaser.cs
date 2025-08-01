4a13858130530cb56e2f5034078348f1d499d461
chuh007 <chhuh007@gmail.com>
2025-08-02 02:03:20 +0900

    feat : Boss

---
+++ Assets/Member/CUH/Code/Combat/Enemies/BossPattern/BossPatternLaser.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Member.CUH.Code.Combat.Enemies.BossPattern
+{
+    public abstract class BossPatternLaser : BossPatternBase
+    {
+        [SerializeField] protected Laser laserPrefab;
+    }
+}

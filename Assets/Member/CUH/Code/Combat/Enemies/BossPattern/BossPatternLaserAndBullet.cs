6af09f4c9ffd096bd4e980fd7b782a14500af19d
chuh007 <chhuh007@gmail.com>
2025-08-02 08:35:26 +0900

    feat : Boss

---
+++ Assets/Member/CUH/Code/Combat/Enemies/BossPattern/BossPatternLaserAndBullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using Member.KDH.Code.Bullet;
+using UnityEngine;
+
+namespace Member.CUH.Code.Combat.Enemies.BossPattern
+{
+    public class BossPatternLaserAndBullet : BossPatternBase
+    {
+        [SerializeField] private Laser laserPrefab;
+        [SerializeField] private float laserScale = 10f;
+        [SerializeField] private Bullet bulletPrefab;
+        [SerializeField] private float bulletSpeed = 3f;
+        [SerializeField] private float bulletLifeTime = 5f;
+        [SerializeField] private float spreadAngle = 15f; // 좌우 퍼짐 각도
+        [SerializeField] private float waitTime = 0.5f; // 레이저와 총알 사이의 시간
+        
+        private Vector3[] _targetPositions = {Vector3.left, Vector3.down, Vector3.right, Vector3.up};
+        
+        public override void UsePattern()
+        {
+            StartCoroutine(LaserAndBulletAttack());
+        }
+
+        private IEnumerator LaserAndBulletAttack()
+        {
+            for (int i = 0; i < fireCount; i++)
+            {
+                Laser laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+                laser.transform.localScale *= laserScale;
+                Vector3 targetPos = _targetPositions[Random.Range(0, _targetPositions.Length)];
+                laser.Shoot(targetPos, transform, laserScale);
+                yield return new WaitForSeconds(waitTime);
+                FireBullet(targetPos);
+                Vector2 leftDir = RotateVector(targetPos, spreadAngle);
+                Vector2 rightDir = RotateVector(targetPos, -spreadAngle);
+
+                FireBullet(leftDir);
+                FireBullet(rightDir);
+                yield return new WaitForSeconds(fireDelay);
+            }
+        }
+        
+        private void FireBullet(Vector2 direction)
+        {
+            var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.Fire(direction.normalized, bulletSpeed);
+            Destroy(bullet.gameObject, bulletLifeTime);
+        }
+
+        private Vector2 RotateVector(Vector2 v, float degrees)
+        {
+            float rad = degrees * Mathf.Deg2Rad;
+            float cos = Mathf.Cos(rad);
+            float sin = Mathf.Sin(rad);
+            return new Vector2(
+                v.x * cos - v.y * sin,
+                v.x * sin + v.y * cos
+            ).normalized;
+        }
+    }
+}
---
+++ Assets/Member/CUH/Code/Combat/Enemies/BossPattern/BossPatternLaserSuper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Member.CUH.Code.Combat.Enemies.BossPattern
+{
+    public class BossPatternLaserSuper : BossPatternLaser
+    {
+        [SerializeField] private float rotationValue = 5f;
+        
+        public override void UsePattern()
+        {
+            StartCoroutine(FireLaserRoutine());
+        }
+        
+        private IEnumerator FireLaserRoutine()
+        {
+            for (int i = 0; i < fireCount; i++)
+            {
+                float rad = rotationValue * i * Mathf.Deg2Rad;
+                Laser laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+                Vector2 toPos = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+                laser.Shoot(toPos.normalized, transform);
+                yield return new WaitForSeconds(fireDelay);
+            }
+        }
+    }
+}

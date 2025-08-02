653ec4579e3d895fb89528abb35e5cca6c887611
Candy <dltkr0424@gmail.com>
2025-08-02 14:48:24 +0900

    feat : MainmenuBGM

---
+++ Assets/Member/ISC/Code/Managers/BGMManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ami.BroAudio;
+using UnityEngine;
+
+namespace Member.ISC.Code.Managers
+{
+    public class BGMManager : MonoBehaviour
+    {
+        [SerializeField] private SoundID soundID;
+
+        private void Start()
+        {
+            soundID.Play();
+        }
+    }
+}

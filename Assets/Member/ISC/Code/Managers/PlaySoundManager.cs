dc2c1a6c88b642170af3873adab142bf638a0d49
Candy <dltkr0424@gmail.com>
2025-08-02 15:20:14 +0900

    feat : video

---
+++ Assets/Member/ISC/Code/Managers/PlaySoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ami.BroAudio;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Member.ISC.Code.Managers
+{
+    public class PlaySoundManager : MonoBehaviour
+    {
+        [SerializeField] private SoundID soundID;
+        [SerializeField] private Slider bgmSlider;
+        [SerializeField] private Slider sfxSlider;
+        [SerializeField] private Slider masterSlider;
+        
+        [SerializeField] private BroAudioType bgm;
+        [SerializeField] private BroAudioType sfx;
+        [SerializeField] private BroAudioType main;
+        
+        private void Start()
+        {
+            soundID.Play();
+        }
+        
+        public void BGM(float volume)
+        {
+            bgmSlider.value = volume;
+            BroAudio.SetVolume(bgm, volume);
+        }
+
+        public void SFX(float volume)
+        {
+            sfxSlider.value = volume;
+            BroAudio.SetVolume(sfx, volume);
+        }
+
+        public void Master(float volume)
+        {
+            masterSlider.value = volume;
+            BroAudio.SetVolume(main, volume);
+        }
+    }
+}
